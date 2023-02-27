04a364f81af42549c69acf7dafe66f2b2c0c9812
JPRichings <j.richings@epcc.ed.ac.uk>
2023-02-27 12:27:09 +0000

    check work files systems avalible on login node

---
+++ tests/env/filesystemsmounted_check.py
@@ -0,0 +1,59 @@
+# Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+from reframe.core.runtime import runtime
+
+
+@rfm.simple_test
+class FileSystemMountCheckWork1(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.descr = 'Ensure file system mounted'
+        self.valid_prog_environs = ['PrgEnv-cray']
+        self.valid_systems = ['archer2:login']
+        self.executable = '[[ -d  /mnt/lustre/a2fs-work1/ ]] && echo "work1-test file system found." '
+        #self.executable_opts = ['']
+        self.maintainers = ['James Richings']
+        self.tags = {'production', 'craype'}
+        self.sanity_patterns = sn.assert_found(r'^work1-test', self.stdout)
+
+
+@rfm.simple_test
+class FileSystemMountCheckWork2(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.descr = 'Ensure file system mounted'
+        self.valid_prog_environs = ['PrgEnv-cray']
+        self.valid_systems = ['archer2:login']
+        self.executable = '[[ -d  /mnt/lustre/a2fs-work2/ ]] && echo "work2-test file system found." '
+        # self.executable_opts = []
+        self.maintainers = ['James Richings']
+        self.tags = {'production', 'craype'}
+        self.sanity_patterns = sn.assert_found(r'^work2-test', self.stdout)
+
+@rfm.simple_test
+class FileSystemMountCheckWork3(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.descr = 'Ensure file system mounted'
+        self.valid_prog_environs = ['PrgEnv-cray']
+        self.valid_systems = ['archer2:login']
+        self.executable = '[[ -d  /mnt/lustre/a2fs-work3/ ]] && echo "work3-test file system found." '
+        # self.executable_opts = []
+        self.maintainers = ['James Richings']
+        self.tags = {'production', 'craype'}
+        self.sanity_patterns = sn.assert_found(r'^work3-test', self.stdout)
+
+@rfm.simple_test
+class FileSystemMountCheckWorknvme(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.descr = 'Ensure file system mounted'
+        self.valid_prog_environs = ['PrgEnv-cray']
+        self.valid_systems = ['archer2:login']
+        self.executable = '[[ -d  /mnt/lustre/a2fs-nvme/ ]] && echo "nvme-test file system found." '
+        # self.executable_opts = []
+        self.maintainers = ['James Richings']
+        self.tags = {'production', 'craype'}
+        self.sanity_patterns = sn.assert_found(r'^nvme-test', self.stdout)
