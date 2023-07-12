d6508fd404d7e8f1b52b14179531ba42631961fb
eleanor-broadway <ebroadwayr@gmail.com>
2023-07-12 15:07:04 +0100

    Replaces old settings file

--- configuration/cirrus_settings.py
+++ configuration/cirrus_settings.py
@@ -1,207 +1,111 @@
-#
-# ReFrame generic settings: for Cirrus
-#
-
-
-class ReframeSettings:
-    refram_module = 'reframe'
-    job_poll_intervals = [1, 2, 3]
-    job_submit_timeout = 60
-    checks_path = ['checks/']
-    checks_path_recurse = True
-
-    site_configuration = {
-        'systems': {
-            'cirrus': {
-                'descr': 'Cirrus',
-                'hostnames': ['indy2'],
-                'modules_system': 'tmod',
-                'partitions': {
-                    'login_ser': {
-                        'scheduler': 'local',
-                        'modules': [],
-                        'access':  [],
-                        'environs': ['PrgEnv-intel18', 'PrgEnv-intel17',
-                                     'PrgEnv-gcc6'],
-                        'descr': 'Login nodes: serial tests',
-                        'max_jobs': 4
-                    },
-    
-                    'compute_ser': {
-                        'scheduler': 'pbs+local',
-                        'modules': [],
-                        'access':  ['-A z04'],
-                        'environs': ['PrgEnv-intel18', 'PrgEnv-intel17', 'PrgEnv-gcc6'],
-                        'descr': 'Compute nodes (Broadwell): serial jobs',
-                        'max_jobs': 10
-                    },
-
-                    'compute_impi': {
-                        'scheduler': 'pbs+impi',
-                        'modules': [],
-                        'access':  ['-A z04','-l place=scatter:excl'],
-                        'environs': ['PrgEnv-intel18-impi', 'PrgEnv-intel17-impi', 'PrgEnv-gcc6-impi'],
-                        'descr': 'Compute nodes (Broadwell): Intel MPI parallel jobs',
-                        'max_jobs': 10
-                    },
-
-                    'compute_mpt': {
-                        'scheduler': 'pbs+hpempt',
-                        'modules': [],
-                        'access':  ['-A z04','-l place=scatter:excl'],
-                        'environs': ['PrgEnv-intel18-mpt', 'PrgEnv-intel17-mpt', 'PrgEnv-gcc6-mpt'],
-                        'descr': 'Compute nodes (Broadwell): HPE MPT parallel jobs',
-                        'max_jobs': 10
-                    },
-                    # This environemnt allows you to lanch scripts containing the MPI command rather
-                    # than direct 'mpiexec_mpt' invocation
-                    'compute_mptloc': {
-                        'scheduler': 'pbs+local',
-                        'modules': [],
-                        'access':  ['-A z04','-l place=scatter:excl'],
-                        'environs': ['PrgEnv-intel18-mpt', 'PrgEnv-intel17-mpt', 'PrgEnv-gcc6-mpt'],
-                        'descr': 'Compute nodes (Broadwell): HPE MPT parallel jobs',
-                        'max_jobs': 10
-                    }
-                }
-            }
-        },
-    
-        'environments': {
-            '*': {
-                'PrgEnv-intel17': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-17'],
-                    'cc': 'icc',
-                    'cxx': 'icpc',
-                    'ftn': 'ifort',
+site_configuration = {
+    'systems': [
+        {
+            'name': 'cirrus',
+            'descr': 'Cirrus',
+            'hostnames': ['cirrus-login1','cirrus-login2','cirrus-login3'],
+            'modules_system': 'tmod4',
+            'partitions': [
+                {
+                    'name': 'login',
+                    'descr': 'Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['gnu','intel'],
                 },
-    
-                'PrgEnv-intel18': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-18/18.0.5.274'],
-                    'cc': 'icc',
-                    'cxx': 'icpc',
-                    'ftn': 'ifort',
+                {
+                   'name': 'compute',
+                   'descr': 'Compute nodes',
+                   'scheduler': 'slurm',
+                   'launcher': 'srun',
+                   'access': ['--hint=nomultithread','--distribution=block:block','--partition=standard'],
+                   'max_jobs': 16,
+                   'environs': ['gnu','intel'],
+                   'resources': [
+                       {
+                           'name': 'qos',
+                           'options': ['--qos={qos}']
+                       },
+                   ]
                 },
-    
-                'PrgEnv-gcc6': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['gcc/6.3.0'],
-                    'cc': 'gcc',
-                    'cxx': 'g++',
-                    'ftn': 'gfortran',
-                },
-
-                'PrgEnv-intel17-impi': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-17', 'intel-mpi-17'],
-                    'cc': 'mpiicc',
-                    'cxx': 'mpiicpc',
-                    'ftn': 'mpiifort',
-                },
-    
-                'PrgEnv-intel18-impi': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-18/18.0.5.274', 'intel-mpi-18'],
-                    'cc': 'mpiicc',
-                    'cxx': 'mpiicpc',
-                    'ftn': 'mpiifort',
-                },
-    
-                'PrgEnv-gcc6-impi': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['gcc/6.3.0', 'intel-mpi-18'],
-                    'cc': 'mpicc',
-                    'cxx': 'mpicxx',
-                    'ftn': 'mpif90',
-                },
-
-                'PrgEnv-intel17-mpt': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-17', 'mpt'],
-                    'variables': {
-                         'MPICC_CC': 'icc',
-                         'MPICXX_CXX': 'icpc'
-                    },
-                    'cc': 'mpicc',
-                    'cxx': 'mpicxx',
-                    'ftn': 'mpif90',
-                },
-    
-                'PrgEnv-intel18-mpt': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['intel-tools-18/18.0.5.274', 'mpt'],
-                    'variables': {
-                         'MPICC_CC': 'icc',
-                         'MPICXX_CXX': 'icpc'
-                    },
-                    'cc': 'mpicc',
-                    'cxx': 'mpicxx',
-                    'ftn': 'mpif90',
-                },
-    
-                'PrgEnv-gcc6-mpt': {
-                    'type': 'ProgEnvironment',
-                    'modules': ['gcc/6.3.0', 'mpt'],
-                    'cc': 'mpicc',
-                    'cxx': 'mpicxx',
-                    'ftn': 'mpif90',
-                }
-            }
+            ]
         }
-    }
+    ],
+    'environments': [
+        {
+            'name': 'gnu',
+            'modules': ['gcc','mpt'],
+            'cc': 'mpicc',
+            'cxx': 'mpicxx',
+            'ftn': 'mpif90',
+            'target_systems': ['cirrus']
+        },
+        {
+            'name': 'intel',
+            'modules': ['mpt','intel-20.4/compilers'],
+            'cc': 'mpicc',
+            'cxx': 'mpicxx',
+            'ftn': 'mpif90',
+            'target_systems': ['cirrus']
+        },
+
+        {
+            'name': 'Default',
+            'cc': 'gcc',
+            'target_systems': ['cirrus']
+        },
 
 
-    logging_config = {
-        'level': 'DEBUG',
-        'handlers': [
-            {
-                'type': 'file',
-                'name': 'reframe.log',
-                'level': 'DEBUG',
-                'format': '[%(asctime)s] %(levelname)s: '
-                          '%(check_info)s: %(message)s',
-                'append': False,
-            },
-
-            # Output handling
-            {
-                'type': 'stream',
-                'name': 'stdout',
-                'level': 'INFO',
-                'format': '%(message)s'
-            },
-            {
-                'type': 'file',
-                'name': 'reframe.out',
-                'level': 'INFO',
-                'format': '%(message)s',
-                'append': False,
-            }
-        ]
-    }
-
-    perf_logging_config = {
-        'level': 'DEBUG',
-        'handlers': [
-            {
-                'type': 'filelog',
-                'prefix': '%(check_system)s/%(check_partition)s',
-                'level': 'INFO',
-                'format': (
-                    '%(asctime)s|reframe %(version)s|'
-                    '%(check_info)s|jobid=%(check_jobid)s|'
-                    '%(check_perf_var)s=%(check_perf_value)s|'
-                    'ref=%(check_perf_ref)s '
-                    '(l=%(check_perf_lower_thres)s, '
-                    'u=%(check_perf_upper_thres)s)|'
-                    '%(check_perf_unit)s'
-                ),
-                'append': True
-            }
-        ]
-    }
-
-
-settings = ReframeSettings()
+    ],
+    'logging': [
+        {
+            'level': 'debug',
+            'handlers': [
+                {
+                    'type': 'stream',
+                    'name': 'stdout',
+                    'level': 'info',
+                    'format': '%(message)s'
+                },
+                {
+                    'type': 'file',
+                    'name': 'reframe.out',
+                    'level': 'info',
+                    'format': '[%(asctime)s] %(check_info)s: %(message)s',
+                    'append': True
+                },
+                {
+                    'type': 'file',
+                    'name': 'reframe.log',
+                    'level': 'debug',
+                    'format': '[%(asctime)s] %(levelname)s %(levelno)s: %(check_info)s: %(message)s',   # noqa: E501
+                    'append': False
+                }
+            ],
+            'handlers_perflog': [
+                {
+                    'type': 'file',
+                    'name': 'reframe_perf.out',
+                    'level': 'info',
+                    'format': '[%(asctime)s] %(check_info)s: %(check_perf_var)s=%(check_perf_value)s (ref=%(check_perf_ref)s;l=%(check_perf_lower_thres)s;u=%(check_perf_upper_thres)s)) %(check_perf_unit)s',
+                    'append': True
+                },
+                {
+                    'type': 'filelog',
+                    'prefix': '%(check_system)s/%(check_partition)s',
+                    'level': 'info',
+                    'format': (
+                        '%(check_job_completion_time)s|reframe %(version)s|'
+                        '%(check_info)s|jobid=%(check_jobid)s|'
+                        '%(check_perf_var)s=%(check_perf_value)s|'
+                        'ref=%(check_perf_ref)s '
+                        '(l=%(check_perf_lower_thres)s, '
+                        'u=%(check_perf_upper_thres)s)|'
+                        '%(check_perf_unit)s'
+                    ),
+                    'append': True
+                }
+            ]
+        }
+    ],
+}
