810ac30329988d432a16b406bdb9b1445167b8c2
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2023-10-20 14:31:44 +0100

    gromacs chages to base class

--- tests/apps/gromacs/gromacs_base.py
+++ tests/apps/gromacs/gromacs_base.py
@@ -3,39 +3,62 @@ import reframe.utility.sanity as sn
 
 
 class GromacsBaseCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self, output_file):
-        super().__init__()
+    #  def __init__(self, output_file):
+    #      super().__init__()
+    #
+    #      self.output_file = output_file
+    #      self.valid_prog_environs = ["PrgEnv-gnu", "gnu", "nvidia-mpi"]
+    #      self.executable = "gmx_mpi"
+    #
+    #      self.keep_files = [self.output_file]
+    #
+    #      self.maintainers = ["a.turner@epcc.ed.ac.uk"]
+    #      self.strict_check = False
+    #      self.use_multithreading = False
+    #      self.extra_resources = {"qos": {"qos": "standard"}}
+    #      self.tags = {"applications", "performance"}
 
-        self.output_file = output_file
-        self.valid_prog_environs = ["PrgEnv-gnu", "gnu", "nvidia-mpi"]
-        self.executable = "gmx_mpi"
+    #  def __init__(self, output_file):
+    #      super().__init__()
+    #
+    #      self.output_file = output_file
 
-        self.keep_files = [self.output_file]
+    valid_prog_environs = ["PrgEnv-gnu", "gnu", "nvidia-mpi"]
+    executable = "gmx_mpi"
 
-        energy = sn.extractsingle(
+    keep_files = ["md.log"]
+
+    maintainers = ["a.turner@epcc.ed.ac.uk"]
+    strict_check = False
+    use_multithreading = False
+    extra_resources = {"qos": {"qos": "standard"}}
+    tags = {"applications", "performance"}
+
+    energy_reference = -12071400.0
+
+    @sanity_function
+    def assert_finished(self):
+        return sn.assert_found(r"Finished mdrun", self.stdout)
+
+    @performance_function(self)
+    def assert_energy(self):
+        return sn.extractsingle(
             r"\s+Potential\s+Kinetic En\.\s+Total Energy"
             r"\s+Conserved En\.\s+Temperature\n"
             r"(\s+\S+){2}\s+(?P<energy>\S+)(\s+\S+){2}\n"
             r"\s+Pressure \(bar\)\s+Constr\. rmsd",
-            self.output_file,
+            self.stdout,
             "energy",
             float,
             item=-1,
         )
-        energy_reference = -12071400.0
 
-        self.sanity_patterns = sn.all(
-            [
-                sn.assert_found("Finished mdrun", self.output_file),
-                sn.assert_reference(energy, energy_reference, -0.01, 0.01),
-            ]
-        )
-
-        self.maintainers = ["a.turner@epcc.ed.ac.uk"]
-        self.strict_check = False
-        self.use_multithreading = False
-        self.extra_resources = {"qos": {"qos": "standard"}}
-        self.tags = {"applications", "performance"}
+    #  self.sanity_patterns = sn.all(
+    #      [
+    #          sn.assert_found("Finished mdrun", self.output_file),
+    #          sn.assert_reference(energy, energy_reference, -0.01, 0.01),
+    #      ]
+    #  )
 
     @performance_function("ns/day", perf_key="perf")
     def extract_perf(self):
