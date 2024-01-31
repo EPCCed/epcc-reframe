e296567d2d0043523ade746d51b1d8449cf32374
BigBalloon8 <raecd123@gmail.com>
2024-01-31 16:34:55 +0000

    gpu updates

--- tests/mlperf/deepcam/gpu.py
+++ tests/mlperf/deepcam/gpu.py
@@ -9,16 +9,15 @@ class DeepCamGPUBenchmark(DeepCamBaseCheck):
     descr = "DeepCam GPU Benchmark"
     modules = ["openmpi/4.1.5-cuda-11.6"]
     num_tasks = None
+    num_gpus = parameter(1 << pow for pow in range(7))
     extra_resources = {
             "qos": {"qos": "gpu"},
-            "gpu": {"num_gpus_per_node": "4"},
         }
     time_limit = "1h"
     env_vars = {
             'OMP_NUM_THREADS': "5",
             "SRUN_CPUS_PER_TASK" : "5",
             "OMPI_MCA_mpi_warn_on_fork": "0",
-            "PARAMS": '"--ntasks=8 --tasks-per-node=4"',
         }
     prerun_cmds = ['eval "$(/work/z043/shared/miniconda3/bin/conda shell.bash hook)"', 
                             "conda activate mlperf-torch", 
@@ -39,13 +38,21 @@ class DeepCamGPUBenchmark(DeepCamBaseCheck):
     
     @run_before('run')
     def set_task_distribution(self):
-        self.job.options = ["--nodes=2", "--exclusive"]  # make sure you change ntasks in PARAMS
-
+        if self.num_gpus <= 4:
+            num_nodes = 1
+        else:
+            if self.num_gpus/4 - float(self.num_gpus//4) == 0:
+                num_nodes = self.num_gpus//4
+            else:
+                num_nodes = num_nodes//4 + 1
+        self.job.options = [f"--nodes={num_nodes}", "--exclusive", f"--gres=gpu:{self.num_gpus if self.num_gpus <= 4 else 4}"]  # make sure you change ntasks in PARAMS
 
 
     @run_after("setup")
     def setup_gpu_options(self):
         """sets up different resources for gpu systems"""
-        self.job.launcher.options.append("${PARAMS}")
+        self.job.launcher.options.append(f'--ntasks={self.num_gpus} --tasks-per-node={self.num_gpus if self.num_gpus <= 4 else 4}')
+
+       
 
 
