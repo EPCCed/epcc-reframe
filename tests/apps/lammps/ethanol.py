7f2603d01b06f817abc3ebc05b7683bc9fadcfe5
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-05-02 14:53:53 +0100

    Refactored lammps tests

--- tests/apps/lammps/ethanol.py
+++ tests/apps/lammps/ethanol.py
@@ -1,64 +1,116 @@
-"""reframe script for lammps ethanol test"""
+"""ReFrame script for LAMMPS ethanol test"""
+
 import reframe as rfm
-import reframe.utility.sanity as sn
+
+from lammps_base import LAMMPSBase
 
 
-class LAMMPSBaseCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self, output_file):
-        super().__init__()
+class LAMMPSBaseEthanol(LAMMPSBase):
+    """ReFrame LAMMPS Ethanol test base class"""
 
-        self.valid_prog_environs = ["PrgEnv-gnu",'intel']
-        self.executable = "lmp"
+    modules = ["lammps"]
+    descr = "LAMMPS Ethanol performance"
+    executable_opts = ["-i in.ethanol"]
 
-        self.keep_files = [output_file]
+    n_nodes = 4
+    num_cpus_per_task = 1
+    time_limit = "20m"
+    env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task)}
 
-        energy = sn.extractsingle(
-            r"\s+11000\s+\S+\s+\S+\s+(?P<energy>\S+)",
-            output_file,
-            "energy",
-            float,
-            item=-1,
-        )
-        energy_reference = 537394.35
+    cores = variable(
+        dict,
+        value={
+            "archer2:compute": 128,
+            "archer2-tds:compute": 128,
+            "cirrus:compute": 36,
+            "cirrus:compute-gpu": 40,
+        },
+    )
 
-        self.sanity_patterns = sn.all(
-            [
-                sn.assert_found("Total wall time", output_file),
-                sn.assert_reference(energy, energy_reference, -0.01, 0.01),
-            ]
-        )
+    ethanol_energy_reference = 537394.35
 
-        self.perf_patterns = {
-            "perf": sn.extractsingle(
-                r"Performance:\s+(?P<perf>\S+)", output_file, "perf", float
-            ),
-        }
+    reference = {
+        "cirrus:compute": {
+            "energy": (ethanol_energy_reference, -0.01, 0.01, "kJ/mol")
+        },
+        "cirrus:compute-gpu": {
+            "energy": (ethanol_energy_reference, -0.01, 0.01, "kJ/mol")
+        },
+        "archer2:compute": {
+            "energy": (ethanol_energy_reference, -0.01, 0.01, "kJ/mol")
+        },
+        "archer2-tds:compute": {
+            "energy": (ethanol_energy_reference, -0.01, 0.01, "kJ/mol")
+        },
+    }
 
-        self.maintainers = ["a.turner@epcc.ed.ac.uk"]
-        self.strict_check = False
-        self.use_multithreading = False
-        self.extra_resources = {"qos": {"qos": "standard"}}
-        self.tags = {"applications", "performance"}
 
-# Needed to be renamed as was conflicting with test of same name in dipole_large.py
 @rfm.simple_test
-class LAMMPSARCHER2LargeCheckEthanol(LAMMPSBaseCheck):
-    def __init__(self):
-        super().__init__("log.lammps")
+class LAMMPSARCHER2EthanolCPU(LAMMPSBaseEthanol):
+    """ReFrame LAMMPS Ethanol test for performance checks"""
 
-        self.valid_systems = ["archer2:compute"]
-        self.descr = "LAMMPS large scale performance test"
-        self.executable_opts = ["-i in.ethanol"]
+    valid_systems = ["archer2:compute", "cirrus:compute"]
+    descr = LAMMPSBaseEthanol.descr + " -- CPU"
 
-        self.modules = ["lammps"]
-        self.num_tasks = 128 * 4
-        self.num_tasks_per_node = 128
-        self.num_cpus_per_task = 1
-        self.time_limit = "20m"
-        self.env_vars = {"OMP_NUM_THREADS": str(self.num_cpus_per_task)}
+    reference["archer2:compute"]["performance"] = (
+        16.800,
+        -0.05,
+        0.05,
+        "ns/day",
+    )
+    reference["archer2-tds:compute"]["performance"] = (
+        1,
+        -0.01,
+        0.01,
+        "ns/day",
+    )
+    reference["cirrus:compute"]["performance"] = (4.8, -0.05, 0.05, "ns/day")
 
-        self.reference = {
-            "archer2:compute": {
-                "perf": (8.627, -0.1, 0.1, "ns/day"),
-            }
-        }
+    @run_after("init")
+    def setup_nnodes(self):
+        """sets up number of tasks per node"""
+        if self.current_system.name in ["archer2"]:
+            self.num_tasks_per_node = 128
+        elif self.current_system.name in ["cirrus"]:
+            self.num_tasks_per_node = 36
+
+    @run_before("run")
+    def setup_resources(self):
+        """sets up number of tasks"""
+        self.num_tasks = self.n_nodes * self.cores.get(
+            self.current_partition.fullname, 1
+        )
+
+
+@rfm.simple_test
+class LAMMPSARCHER2EthanolGPU(LAMMPSBaseEthanol):
+    """ReFrame LAMMPS Ethanol test for performance checks"""
+
+    valid_systems = ["cirrus:compute-gpu"]
+    descr = LAMMPSBaseEthanol.descr + " -- GPU"
+    modules = ["lammps-gpu"]
+    extra_resources = {
+        "qos": {"qos": "short"},
+        "gpu": {"num_gpus_per_node": "4"},
+    }
+    env_vars["PARAMS"] = '"--exclusive --ntasks=40 --tasks-per-node=40"'
+
+    n_nodes = 1
+    num_tasks = None
+    num_cpus_per_task = None
+
+    executable_opts = LAMMPSBaseEthanol.executable_opts + ["-sf gpu -pk gpu 4"]
+
+    reference["cirrus:compute-gpu"]["performance"] = (
+        9.4,
+        -0.05,
+        0.05,
+        "ns/day",
+    )
+
+    @run_after("setup")
+    def setup_gpu_options(self):
+        """sets up different resources for gpu systems"""
+        # Cirrus slurm demands it be done this way.
+        # Trying to add $PARAMS directly to job.launcher.options fails.
+        self.job.launcher.options.append("${PARAMS}")
---
+++ tests/apps/lammps/lammps_base.py
@@ -0,0 +1,43 @@
+"""ReFrame base module for LAMMPS tests"""
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+class LAMMPSBase(rfm.RunOnlyRegressionTest):
+    """ReFrame base class for LAMMPS tests"""
+
+    valid_prog_environs = ["PrgEnv-gnu", "intel", "nvidia-mpi"]
+    executable = "lmp"
+    extra_resources = {"qos": {"qos": "standard"}}
+
+    keep_files = ["log.lammps"]
+
+    maintainers = ["r.apostolo@epcc.ed.ac.uk"]
+    strict_check = True
+    tags = {"applications", "performance"}
+
+    @sanity_function
+    def assert_finished(self):
+        """Sanity check that simulation finished successfully"""
+        return sn.assert_found(r"Total wall time", self.keep_files[0])
+
+    @performance_function("kJ/mol", perf_key="energy")
+    def extract_energy(self):
+        """Extract value of system energy for performance check"""
+        return sn.extractsingle(
+            r"\s+11000\s+\S+\s+\S+\s+(?P<energy>\S+)",
+            self.keep_files[0],
+            "energy",
+            float,
+            item=-1,
+        )
+
+    @performance_function("ns/day", perf_key="performance")
+    def extract_perf(self):
+        """Extract performance value to compare with reference value"""
+        return sn.extractsingle(
+            r"Performance:\s+(?P<perf>\S+)",
+            self.keep_files[0],
+            "perf",
+            float,
+        )
