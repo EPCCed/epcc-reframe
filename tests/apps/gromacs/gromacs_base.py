163baee1ebb0fbcc2767ae635ad857141d5c8c94
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-05-08 11:35:43 +0100

    Merged CPU and GPU checks into one module, 2 different child classes

--- tests/apps/gromacs/gromacs_base.py
+++ tests/apps/gromacs/gromacs_base.py
@@ -1,4 +1,3 @@
-"""ReFrame base module for GROMACS tests"""
 import reframe as rfm
 import reframe.utility.sanity as sn
 
@@ -8,21 +7,21 @@ class GromacsBaseCheck(rfm.RunOnlyRegressionTest):
 
     valid_prog_environs = ["PrgEnv-gnu", "gnu", "nvidia-mpi"]
     executable = "gmx_mpi"
+    extra_resources = {"qos": {"qos": "standard"}}
 
     keep_files = ["md.log"]
 
     maintainers = ["r.apostolo@epcc.ed.ac.uk"]
     strict_check = True
+    use_multithreading = False
     tags = {"applications", "performance"}
 
     @sanity_function
     def assert_finished(self):
-        """Sanity check that simulation finished successfully"""
         return sn.assert_found(r"Finished mdrun", self.keep_files[0])
 
     @performance_function("kJ/mol", perf_key="energy")
-    def extract_energy(self):
-        """Extracts value of system energy for performance check"""
+    def assert_energy(self):
         return sn.extractsingle(
             r"\s+Potential\s+Kinetic En\.\s+Total Energy"
             r"\s+Conserved En\.\s+Temperature\n"
@@ -34,9 +33,8 @@ class GromacsBaseCheck(rfm.RunOnlyRegressionTest):
             item=-1,
         )
 
-    @performance_function("ns/day", perf_key="performance")
+    @performance_function("ns/day", perf_key="perf")
     def extract_perf(self):
-        """Extract performance value to compare with reference value"""
         return sn.extractsingle(
             r"Performance:\s+(?P<perf>\S+)",
             self.keep_files[0],
