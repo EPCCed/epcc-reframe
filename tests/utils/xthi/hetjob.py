9dff88e823de9e577ace1e6161900209492fce2c
Rui Apóstolo <ruiapostolo@gmail.com>
2024-06-28 11:40:31 +0100

    xthi-hetjob python-black changes

--- tests/utils/xthi/hetjob.py
+++ tests/utils/xthi/hetjob.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import os
-
 import reframe as rfm
 import reframe.utility.sanity as sanity
 import reframe.utility.udeps as depends
@@ -17,6 +15,7 @@ These are really by way of smoke tests, as there's no attempt to
 check the resulting placement at the moment.
 """
 
+
 @rfm.simple_test
 class SharedCommWorldTest(rfm.RunOnlyRegressionTest):
 
@@ -24,28 +23,32 @@ class SharedCommWorldTest(rfm.RunOnlyRegressionTest):
     SLURM hetjob with 3 nodes (no OpenMP) as per frist example
     https://docs.archer2.ac.uk/user-guide/scheduler/#heterogeneous-jobs
     """
+
     def __init__(self):
         self.descr = "SLURM hetjob for xthi shared MPI_COM_WORLD"
         self.valid_systems = ["archer2:compute"]
         self.valid_prog_environs = ["*"]
         self.modules = ["xthi"]
-        
+
         # Utter, utter kludge
         # 1 + 2 nodes; 8 + 2x4 MPI tasks
-        self.hetgroup0 = "--het-group=0 --nodes=1 --ntasks=8 --ntasks-per-node=8 xthi"
-        self.hetgroup1 = "--het-group=1 --nodes=2 --ntasks=8 --ntasks-per-node=4 xthi"
+        self.hetgroup0 = (
+            "--het-group=0 --nodes=1 --ntasks=8 --ntasks-per-node=8 xthi"
+        )
+        self.hetgroup1 = (
+            "--het-group=1 --nodes=2 --ntasks=8 --ntasks-per-node=4 xthi"
+        )
         self.executable = self.hetgroup0 + " : " + self.hetgroup1
-        
-        self.time_limit ="2m"
+
+        self.time_limit = "2m"
         # Kludge three nodes because we can't set the number of nodes explicitly
         self.num_tasks = 3
         self.num_tasks_per_node = 1
         self.env_vars = {"OMP_PLACES": "cores"}
-        self.extra_resources = {'qos': {'qos': 'standard'}}
+        self.extra_resources = {"qos": {"qos": "standard"}}
 
     @sanity_function
     def sanity_check_run(self):
-     
         return sanity.assert_found("Node summary", self.stdout)
 
 
@@ -57,28 +60,35 @@ class SharedCommWorldWithOpenMPTest(rfm.RunOnlyRegressionTest):
     the mixed MPI/OpenMP example at
     https://docs.archer2.ac.uk/user-guide/scheduler/#heterogeneous-jobs
     """
+
     def __init__(self):
         self.descr = "SLURM hetjob for shared MPI_COM_WORLD with OpenMP"
         self.valid_systems = ["archer2:compute"]
         self.valid_prog_environs = ["*"]
         self.modules = ["xthi"]
-        
+
         # Two nodes with 8 MPI tasks per node
-        self.shared_args = " --nodes=1 --ntasks=8 --tasks-per-node=8 --cpus-per-task=16"
-        self.openmp0     = " --export=all,OMP_NUM_THREADS=16"
-        self.openmp1     = " --export=all,OMP_NUM_THREADS=1"
-        self.hetgroup0   = "--het-group=0" + self.shared_args + self.openmp0 + " xthi"
-        self.hetgroup1   = "--het-group=1" + self.shared_args + self.openmp1 + " xthi"
-        self.executable  = self.hetgroup0 + " : " + self.hetgroup1
-        
-        self.time_limit ="2m"
-        
+        self.shared_args = (
+            " --nodes=1 --ntasks=8 --tasks-per-node=8 --cpus-per-task=16"
+        )
+        self.openmp0 = " --export=all,OMP_NUM_THREADS=16"
+        self.openmp1 = " --export=all,OMP_NUM_THREADS=1"
+        self.hetgroup0 = (
+            "--het-group=0" + self.shared_args + self.openmp0 + " xthi"
+        )
+        self.hetgroup1 = (
+            "--het-group=1" + self.shared_args + self.openmp1 + " xthi"
+        )
+        self.executable = self.hetgroup0 + " : " + self.hetgroup1
+
+        self.time_limit = "2m"
+
         # Kludge two nodes because we can't set the number of nodes explicitly
         self.num_tasks = 2
         self.num_tasks_per_node = 1
-        
+
         self.env_vars = {"OMP_PLACES": "cores"}
-        self.extra_resources = {'qos': {'qos': 'standard'}}
+        self.extra_resources = {"qos": {"qos": "standard"}}
 
     @run_before("run")
     def unset_omp_num_threads(self):
@@ -86,6 +96,4 @@ class SharedCommWorldWithOpenMPTest(rfm.RunOnlyRegressionTest):
 
     @sanity_function
     def sanity_check_run(self):
-
         return sanity.assert_found("Node summary", self.stdout)
-
