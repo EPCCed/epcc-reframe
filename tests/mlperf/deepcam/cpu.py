a3e8c69784e62be81379de2eb4c72bb20fb8e4c8
Rui Apóstolo <ruiapostolo@gmail.com>
2024-06-28 13:41:00 +0100

    mlperf/deepcam style

--- tests/mlperf/deepcam/cpu.py
+++ tests/mlperf/deepcam/cpu.py
@@ -1,46 +1,65 @@
+#!/usr/bin/env python3
+
+"""Deepcam CPU tests"""
+
 import reframe as rfm
-import reframe.utility.sanity as sn
+
 from deepcam_base import DeepCamBaseCheck
+
 
 @rfm.simple_test
 class DeepCamCPUCheck(DeepCamBaseCheck):
+    """Deepcam CPU test class"""
+
     descr = "DeepCam CPU Benchmark"
-    valid_systems = ['archer2:compute', 'cirrus:compute']
-    num_task_per_node=8
-    executable = 'python'
+    valid_systems = ["archer2:compute", "cirrus:compute"]
+    num_task_per_node = 8
+    executable = "python"
     num_tasks = 32
     time_limit = "2h"
-    reference = {"archer2:compute": {"Throughput": (200, -0.05, 0.10, "images/s"),
-                                     "Communication Time": (0.3, -0.1, 0.1, "s"),
-                                     "Epoch Length": (500, -0.05, 0.05, "s"),
-                                     "Total IO Time": (8.4, -0.1, 0.1, "s")
-                                    }
-                }
+    reference = {
+        "archer2:compute": {
+            "Throughput": (200, -0.05, 0.10, "images/s"),
+            "Communication Time": (0.3, -0.1, 0.1, "s"),
+            "Epoch Length": (500, -0.05, 0.05, "s"),
+            "Total IO Time": (8.4, -0.1, 0.1, "s"),
+        }
+    }
+
     @run_after("init")
     def setup_systems(self):
+        """Environement setup"""
         if self.current_system.name in ["archer2"]:
-            self.valid_prog_environs = ['PrgEnv-gnu']
+            self.valid_prog_environs = ["PrgEnv-gnu"]
             self.num_cpus_per_task = 16
             self.env_vars = {
-                'OMP_NUM_THREADS': str(self.num_cpus_per_task),
-                "SRUN_CPUS_PER_TASK" : str(self.num_cpus_per_task)
+                "OMP_NUM_THREADS": str(self.num_cpus_per_task),
+                "SRUN_CPUS_PER_TASK": str(self.num_cpus_per_task),
             }
-            self.prerun_cmds = ['eval "$(/work/z043/shared/miniconda3/bin/conda shell.bash hook)"', "conda activate mlperf-torch-cpu"]
-            self.executable_opts = ["/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/train.py", 
-                                    " --config", "/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/configs/archer2benchmark_config.yaml"
-                                    ]
-
+            self.prerun_cmds = [
+                'eval "$(/work/z043/shared/miniconda3/bin/conda shell.bash hook)"',
+                "conda activate mlperf-torch-cpu",
+            ]
+            self.executable_opts = [
+                "/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/train.py",
+                " --config",
+                "/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/configs/archer2benchmark_config.yaml",
+            ]
 
         elif self.current_system.name in ["cirrus"]:
-            self.valid_prog_environs = ['Default']
+            self.valid_prog_environs = ["Default"]
             self.modules = ["openmpi/4.1.5"]
             self.num_cpus_per_task = 36
             self.env_vars = {
-                'OMP_NUM_THREADS': str(self.num_cpus_per_task),
-                "OMPI_MCA_mpi_warn_on_fork": 0
+                "OMP_NUM_THREADS": str(self.num_cpus_per_task),
+                "OMPI_MCA_mpi_warn_on_fork": 0,
             }
-            self.prerun_cmds = ['eval "$(/work/z043/shared/miniconda3/bin/conda shell.bash hook)"', "conda activate mlperf-torch"]
-            self.executable_opts = ["/work/z043/shared/chris-ml-intern//ML_HPC/DeepCAM/Torch/train.py",
-                                    " --config", "/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/configs/cirrusbenchmark_config.yaml"
-                                    ]
-
+            self.prerun_cmds = [
+                'eval "$(/work/z043/shared/miniconda3/bin/conda shell.bash hook)"',
+                "conda activate mlperf-torch",
+            ]
+            self.executable_opts = [
+                "/work/z043/shared/chris-ml-intern//ML_HPC/DeepCAM/Torch/train.py",
+                " --config",
+                "/work/z043/shared/chris-ml-intern/ML_HPC/DeepCAM/Torch/configs/cirrusbenchmark_config.yaml",
+            ]
--- tests/mlperf/deepcam/eidf_gpu.py
+++ tests/mlperf/deepcam/eidf_gpu.py
@@ -1,7 +1,7 @@
-import os
-import random
-import string
-import yaml
+#!/usr/bin/env python3
+
+"""Deepcam tests for EIDF"""
+
 import copy
 
 import reframe as rfm
@@ -12,9 +12,7 @@ from deepcam_base import DeepCamBaseCheck
 base_k8s_pod = {
     "apiVersion": "v1",
     "kind": "Pod",
-    "metadata": {
-        "generateName": "---CHANGE-NAME---"
-    },
+    "metadata": {"generateName": "---CHANGE-NAME---"},
     "spec": {
         "restartPolicy": "Never",
         "containers": [
@@ -22,76 +20,53 @@ base_k8s_pod = {
                 "name": "---CHANGE-NAME---",
                 "image": "bigballoon8/mlperf-epcc",
                 "workingDir": "---PATH-WORKDIR---",
-                "command": [
-                    "---ADD-CMDs---"
-                ],
-                "args": [
-                    "---ADD-ARGS---"
-                ],
-                "env": [
-                    {"name": "OMP_NUM_THREADS", "value": "8"}
-                ],
+                "command": ["---ADD-CMDs---"],
+                "args": ["---ADD-ARGS---"],
+                "env": [{"name": "OMP_NUM_THREADS", "value": "8"}],
                 "resources": {
-                    "requests": {
-                        "cpu": 32,
-                        "memory": "64Gi"
-                    },
+                    "requests": {"cpu": 32, "memory": "64Gi"},
                     "limits": {
                         "cpu": 32,
                         "memory": "64Gi",
-                        "nvidia.com/gpu": "---CHANGE INT--"
-                    }
+                        "nvidia.com/gpu": "---CHANGE INT--",
+                    },
                 },
                 "volumeMounts": [
-                    {
-                        "mountPath": "/mnt/ceph_rbd",
-                        "name": "volume"
-                    },
-                    {
-                        "mountPath": "/dev/shm",
-                        "name": "devshm"
-                    }
-                ]
+                    {"mountPath": "/mnt/ceph_rbd", "name": "volume"},
+                    {"mountPath": "/dev/shm", "name": "devshm"},
+                ],
             }
         ],
-        "nodeSelector": {
-            "nvidia.com/gpu.product": "---CHANGE-NAME---"
-        },
+        "nodeSelector": {"nvidia.com/gpu.product": "---CHANGE-NAME---"},
         "volumes": [
             {
                 "name": "volume",
-                "persistentVolumeClaim": {
-                    "claimName": "---CHANGE-NAME---"
-                }
+                "persistentVolumeClaim": {"claimName": "---CHANGE-NAME---"},
             },
-            {
-                "name": "devshm",
-                "emptyDir": {
-                    "medium": "Memory"
-                }
-            }
-            
-        ]
-    }
+            {"name": "devshm", "emptyDir": {"medium": "Memory"}},
+        ],
+    },
 }
+
 
 @rfm.simple_test
 class DeepCAMGPUServiceBenchmark(DeepCamBaseCheck):
+    """Deepcam test class"""
+
     valid_prog_environs = ["*"]
-    valid_systems = ['eidf:gpu-service']
-    
-    # num_gpus = parameter(1 << pow for pow in range(3))
-    #num_gpus = variable(int, value=4) 
+    valid_systems = ["eidf:gpu-service"]
+
     num_gpus = parameter([4])
-    lbs = 1 #parameter([0])
+    lbs = 1
     dataset = parameter(["mini"])
-    
-    #node_type = parameter(["NVIDIA-A100-SXM4-40GB", "NVIDIA-H100-80GB-HBM3"])
-    node_type = parameter(["NVIDIA-A100-SXM4-40GB"])  
+
+    # node_type = parameter(["NVIDIA-A100-SXM4-40GB", "NVIDIA-H100-80GB-HBM3"])
+    node_type = parameter(["NVIDIA-A100-SXM4-40GB"])
+    job_name = "mlperf-deepcam-"
 
     @run_after("init")
     def executable_setup(self):
-        self.job_name = f"mlperf-deepcam-"
+        """setup variables"""
         if self.dataset == "mini":
             dset_path = "/mnt/ceph_rbd/deepcam/mini/deepcam-data-n512"
             pvc = "deepcam-mini"
@@ -99,31 +74,48 @@ class DeepCAMGPUServiceBenchmark(DeepCamBaseCheck):
             dset_path = "/mnt/ceph_rbd/gridftp-save/deepcam/All-Hist"
             pvc = "deepcam-pvc"
         else:
-            raise ValueError(f"Dataset type '{self.dataset}' is not supported please select full|mini")
+            raise ValueError(
+                f"Dataset type '{self.dataset}' is not supported please select full|mini"
+            )
         pod_info = copy.deepcopy(base_k8s_pod)
         pod_info["metadata"]["generateName"] = self.job_name
-        pod_info["spec"]["containers"][0]["name"] = self.job_name[:-1] # remove '...-' at the end of str
-        pod_info["spec"]["containers"][0]["workingDir"] = "/workspace/ML_HPC/DeepCAM"
+        pod_info["spec"]["containers"][0]["name"] = self.job_name[
+            :-1
+        ]  # remove '...-' at the end of str
+        pod_info["spec"]["containers"][0][
+            "workingDir"
+        ] = "/workspace/ML_HPC/DeepCAM"
         pod_info["spec"]["containers"][0]["command"] = ["torchrun"]
         pod_info["spec"]["containers"][0]["args"] = [
-            f"--nproc_per_node={self.num_gpus}", 
+            f"--nproc_per_node={self.num_gpus}",
             "/workspace/ML_HPC/DeepCAM/Torch/train.py",
-            "-lbs", f"{self.lbs}",
-            "-c", "/workspace/ML_HPC/DeepCAM/Torch/config.yaml",
-            "--data_dir", dset_path #"/mnt/ceph_rbd/deepcam/mini/deepcam-data-n512" #"/mnt/ceph_rbd/gridftp-save/deepcam/All-Hist", 
+            "-lbs",
+            f"{self.lbs}",
+            "-c",
+            "/workspace/ML_HPC/DeepCAM/Torch/config.yaml",
+            "--data_dir",
+            dset_path,  # "/mnt/ceph_rbd/deepcam/mini/deepcam-data-n512" #"/mnt/ceph_rbd/gridftp-save/deepcam/All-Hist",
         ]
-        #print(" ".join(pod_info["spec"]["containers"][0]["args"]))
-        
-        pod_info["spec"]["containers"][0]["resources"]["limits"]["nvidia.com/gpu"] = self.num_gpus
-        pod_info["spec"]["nodeSelector"]["nvidia.com/gpu.product"] = self.node_type
-        pod_info["spec"]["volumes"][0]["persistentVolumeClaim"]["claimName"] = pvc
-        
+        # print(" ".join(pod_info["spec"]["containers"][0]["args"]))
+
+        pod_info["spec"]["containers"][0]["resources"]["limits"][
+            "nvidia.com/gpu"
+        ] = self.num_gpus
+        pod_info["spec"]["nodeSelector"][
+            "nvidia.com/gpu.product"
+        ] = self.node_type
+        pod_info["spec"]["volumes"][0]["persistentVolumeClaim"][
+            "claimName"
+        ] = pvc
+
         self.k8s_config = pod_info
 
-    
     @performance_function("W", perf_key="Avg GPU Power Draw:")
     def extract_gpu_power_draw(self):
-        return sn.extractsingle(r"Avg GPU Power Draw: (.*)", self.stdout, tag= 1, conv=float)
+        """Extracts gpu power draw"""
+        return sn.extractsingle(
+            r"Avg GPU Power Draw: (.*)", self.stdout, tag=1, conv=float
+        )
 
     @run_before("performance")
     def set_perf_variables(self):
@@ -132,15 +124,5 @@ class DeepCAMGPUServiceBenchmark(DeepCamBaseCheck):
             "Epoch Length": self.extract_epoch_length(),
             "Communication Time": self.extract_communication(),
             "Avg GPU Power Draw": self.extract_gpu_power_draw(),
-            "Total IO Time": self.extract_IO()
+            "Total IO Time": self.extract_IO(),
         }
-        
-        
-        
-        
-
-    
-    
-    
-    
-    
