6d83e522136a95d72152dd12067c7c8057b35d67
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-07-22 11:47:44 +0100

    XCompact3D syntax and style

--- tests/apps/xcompact3d/xcompact3d.py
+++ tests/apps/xcompact3d/xcompact3d.py
@@ -1,59 +1,57 @@
-# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
-# ReFrame Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+#!/usr/bin/env python3
+
+"""Reframe test for XCompact3D"""
+
+# Based on original work from:
+#   Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+#   ReFrame Project Developers. See the top-level LICENSE file for details.
+#   SPDX-License-Identifier: BSD-3-Clause
 
 import reframe as rfm
 import reframe.utility.sanity as sn
-import os 
-import re
-import json
+
 
 @rfm.simple_test
-class xcompact3dLargeTest(rfm.RegressionTest):
+class XCompact3DLargeTest(rfm.RegressionTest):
+    """XCompact 3D Large Test"""
 
-    valid_systems = ['archer2:compute']
-    valid_prog_environs = ['PrgEnv-gnu']
+    valid_systems = ["archer2:compute"]
+    valid_prog_environs = ["PrgEnv-gnu"]
 
-    tags = {'performance','largescale','applications'}
+    tags = {"performance", "largescale", "applications"}
 
-    def __init__(self,**kwds):
+    num_nodes = 1024
+    num_tasks_per_node = 128
+    num_cpus_per_task = 1
+    num_tasks = num_nodes * num_tasks_per_node * num_cpus_per_task
 
-        super().__init__()
-        self.num_nodes = 1024
-        self.num_tasks_per_node = 128
-        self.num_cpus_per_task = 1
-        self.num_tasks = self.num_nodes * self.num_tasks_per_node * self.num_cpus_per_task
+    env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task)}
 
-        self.env_vars = {
-            "OMP_NUM_THREADS": str(self.num_cpus_per_task)
-        }
+    time_limit = "1h"
+    build_system = "CMake"
+    build_system.ftn = "ftn"
+    prebuild_cmds = [
+        "git clone https://github.com/xcompact3d/Incompact3d.git",
+        "cd Incompact3d",
+    ]
+    builddir = "Incompact3d"
+    executable = "Incompact3d/bin/xcompact3d"
+    executable_opts = ["large.i3d"]
+    modules = ["cmake/3.29.4"]
 
-        self.modules = ["cmake/3.29.4"]
+    reference = {"archer2:compute": {"steptime": (6.3, -0.2, 0.2, "seconds")}}
 
-        self.time_limit = '1h'
-        self.build_system = 'CMake'
-        self.build_system.ftn="ftn"
-        self.prebuild_cmds = ["git clone https://github.com/xcompact3d/Incompact3d.git", "cd Incompact3d"]
-        self.builddir = "Incompact3d"
-        self.executable = 'Incompact3d/bin/xcompact3d'
-        self.executable_opts = ['large.i3d']
+    @sanity_function
+    def assert_finished(self):
+        """Sanity check that simulation finished successfully"""
+        return sn.assert_found("Good job", self.stdout)
 
-        self.sanity_patterns = sn.all([
-            sn.assert_found('Good job', self.stdout)
-        ])
-
-        self.perf_patterns = {
-            'steptime': sn.extractsingle(r'Averaged time per step \(s\):\s+(?P<steptime>\S+)',
-                                     self.stdout, 'steptime', float)
-        }
-        
-
-        self.reference = {
-                'archer2:compute': {
-                    'steptime': (6.3, -0.2, 0.2, 's')
-                }
-        }
-
-        
-
+    @performance_function("seconds", perf_key="performance")
+    def extract_perf(self):
+        """Extract performance value to compare with reference value"""
+        return sn.extractsingle(
+            r"Averaged time per step \(s\):\s+(?P<steptime>\S+)",
+            self.stdout,
+            "steptime",
+            float,
+        )
