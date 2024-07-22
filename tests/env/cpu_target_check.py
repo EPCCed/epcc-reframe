7017c4895606aaad82ae9d9d135f6f2131df0c09
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-07-22 11:53:16 +0100

    CPU target check style and syntax

--- tests/env/cpu_target_check.py
+++ tests/env/cpu_target_check.py
@@ -1,7 +1,10 @@
-# Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
-# ReFrame Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+#!/usr/bin/env python3
+"""Reframe test to check that CPU target environment variable is correctly set"""
+
+# Based on work from:
+#   Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+#   ReFrame Project Developers. See the top-level LICENSE file for details.
+#   SPDX-License-Identifier: BSD-3-Clause
 
 import reframe as rfm
 import reframe.utility.sanity as sn
@@ -9,14 +12,17 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class CrayCPUTargetTest(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'Checks whether CRAY_CPU_TARGET is set'
-        self.valid_systems = ['archer2:login']
-        self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
-                                    'PrgEnv-aocc']
-        self.sourcesdir = None
-        self.executable = 'echo CRAY_CPU_TARGET=$CRAY_CPU_TARGET'
-        self.sanity_patterns = sn.assert_found(r'CRAY_CPU_TARGET=\S+',
-                                               self.stdout)
+    """Checks that CPU target is correctly set"""
 
-        self.tags = {'production', 'maintenance', 'craype'}
+    descr = "Checks whether CRAY_CPU_TARGET is set"
+    valid_systems = ["archer2:login"]
+    valid_prog_environs = ["PrgEnv-cray", "PrgEnv-gnu", "PrgEnv-aocc"]
+    sourcesdir = None
+    executable = "echo CRAY_CPU_TARGET=$CRAY_CPU_TARGET"
+
+    tags = {"production", "maintenance", "craype"}
+
+    @sanity_function
+    def assert_finished(self):
+        """Sanity check that CPU_CRAY_TARGET is set"""
+        return sn.assert_found(r"CRAY_CPU_TARGET=\S+", self.stdout)
