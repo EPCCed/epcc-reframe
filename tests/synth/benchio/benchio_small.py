312a428e4cbbe7d13900d795a11c23d043734e20
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2024-08-09 13:48:31 +0100

    synth

--- tests/synth/benchio/benchio_small.py
+++ tests/synth/benchio/benchio_small.py
@@ -1,110 +1,137 @@
-# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
-# ReFrame Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+#!/usr/bin/env python3
+"""
+Benchio Input/Output test
+
+The test is parameterized on the folder where to write with parameter 'write_dir_prefix'.
+You might want to choose a folder for each filesystem.
+Currently test data is being written in the z19 shared directory on all 4 work filesystems
+so this test must be run by a member of the z19 project on ARCHER2.
+
+# Based on work by:
+#   Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+#   ReFrame Project Developers. See the top-level LICENSE file for details.
+#   SPDX-License-Identifier: BSD-3-Clause
+"""
+
+import os
 
 import reframe as rfm
 import reframe.utility.sanity as sn
-import os 
-import re
-import json
 
-'''
-Benchio Input/Output test
-
-The test is parameterized on the folder where to write with parameter 'write_dir_prefix'. You might want to choose a folder for each filesystem.
-Currently test data is being written in the z19 shared directory on all 4 work filesystems so this test must be run by a member of the z19
-project on ARCHER2.
-'''
 
 @rfm.simple_test
-class benchioSmallTest(rfm.RegressionTest):
+class BenchioSmallTest(rfm.RegressionTest):
+    """Benchio small test class"""
 
-    valid_systems = ['archer2:compute']
-    valid_prog_environs = ['PrgEnv-gnu']
+    valid_systems = ["archer2:compute"]
+    valid_prog_environs = ["PrgEnv-gnu"]
 
-    tags = {'performance','short','io'}
+    tags = {"performance", "short", "io"}
 
-    num_nodes = parameter( [1,2] )
+    num_nodes = parameter([1, 2])
 
     write_dir_prefix = parameter(
         [
-        '/mnt/lustre/a2fs-work1/work/z19/z19/shared',
-        '/mnt/lustre/a2fs-work2/work/z19/z19/shared',
-        '/mnt/lustre/a2fs-work3/work/z19/z19/shared',
-        '/mnt/lustre/a2fs-work4/work/z19/z19/shared',
-        '/mnt/lustre/a2fs-nvme/work/z19/z19/shared'
+            "/mnt/lustre/a2fs-work1/work/z19/z19/shared",
+            "/mnt/lustre/a2fs-work2/work/z19/z19/shared",
+            "/mnt/lustre/a2fs-work3/work/z19/z19/shared",
+            "/mnt/lustre/a2fs-work4/work/z19/z19/shared",
+            "/mnt/lustre/a2fs-nvme/work/z19/z19/shared",
         ]
     )
 
-    def set_references_per_node(self):
+    executable_opts = ("1260 1260 1260 global mpiio hdf5 fsync").split()
+    num_tasks = 128 * num_nodes
+    num_tasks_per_node = 128
+    num_cpus_per_task = 1
 
+    env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task)}
+
+    prerun_cmds = ["source create_striped_dirs.sh", "module remove darshan"]
+    postrun_cmds = ["source delete_dirs.sh"]
+    time_limit = "1h"
+    build_system = "CMake"
+    build_system.ftn = "ftn"
+    modules = ["cray-hdf5-parallel"]
+
+    @run_after("setup")
+    def set_references_per_node(self):
+        """set reference values"""
         if self.num_nodes == 1:
             self.reference = {
-                'archer2:compute': {
-                'fullstriped_hdf5': ( 1.2, -0.4, 0.4 ,'GB/s'),
-                'unstriped_hdf5': ( 0.5, -0.3, 0.3 ,'GB/s'),
-                'fullstriped_mpiio': ( 1.2, -0.4, 0.4 ,'GB/s'),
-                'unstriped_mpiio': ( 0.6, -0.3, 0.3 ,'GB/s')
-                    }
-                    }
+                "archer2:compute": {
+                    "fullstriped_hdf5": (1.2, -0.4, 0.4, "GB/s"),
+                    "unstriped_hdf5": (0.5, -0.3, 0.3, "GB/s"),
+                    "fullstriped_mpiio": (1.2, -0.4, 0.4, "GB/s"),
+                    "unstriped_mpiio": (0.6, -0.3, 0.3, "GB/s"),
+                }
+            }
         else:
-            if self.num_nodes==2:
+            if self.num_nodes == 2:
                 self.reference = {
-                'archer2:compute': {
-                'fullstriped_hdf5': ( 2.4, -0.4, 0.4 ,'GB/s'),
-                'unstriped_hdf5': ( 0.5, -0.5, 0.5 ,'GB/s'),
-                'fullstriped_mpiio': ( 2.0, -0.4, 0.4 ,'GB/s'),
-                'unstriped_mpiio': ( 0.5, -0.5, 0.5 ,'GB/s')
+                    "archer2:compute": {
+                        "fullstriped_hdf5": (2.4, -0.4, 0.4, "GB/s"),
+                        "unstriped_hdf5": (0.5, -0.5, 0.5, "GB/s"),
+                        "fullstriped_mpiio": (2.0, -0.4, 0.4, "GB/s"),
+                        "unstriped_mpiio": (0.5, -0.5, 0.5, "GB/s"),
                     }
-                    }
+                }
             else:
-                raise Exception("References are defined for calculations with 1 or 2 nodes")
+                raise ValueError("References are defined for calculations with 1 or 2 nodes")
 
+    @performance_function("GiB/s")
+    def extract_striped_hdf5(self):
+        """Extract striped hdf5 rate"""
+        return sn.extractsingle(
+            r"Writing to fullstriped/hdf5\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)",
+            self.stdout,
+            1,
+            float,
+        )
 
-    def __init__(self,**kwds):
+    @performance_function("GiB/s")
+    def extract_unstriped_hdf5(self):
+        """Extract unstriped hdf5 rate"""
+        return sn.extractsingle(
+            r"Writing to unstriped/hdf5\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)",
+            self.stdout,
+            1,
+            float,
+        )
 
-        super().__init__()
-        self.executable_opts = ('1260 1260 1260 global mpiio hdf5 fsync').split()
-        self.num_tasks = 128 * self.num_nodes
-        self.num_tasks_per_node = 128
-        self.num_cpus_per_task = 1
+    @performance_function("GiB/s")
+    def extract_unstriped_mpiio(self):
+        """Extract unstriped mpiio rate"""
+        return sn.extractsingle(
+            r"Writing to unstriped/mpiio\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)",
+            self.stdout,
+            1,
+            float,
+        )
 
-        self.env_vars = {"OMP_NUM_THREADS": str(self.num_cpus_per_task)}
+    @performance_function("GiB/s")
+    def extract_striped_mpiio(self):
+        """Extract striped mpiio rate"""
+        return sn.extractsingle(
+            r"Writing to fullstriped/mpiio\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)",
+            self.stdout,
+            1,
+            float,
+        )
 
-        self.prerun_cmds  = ['source create_striped_dirs.sh', 'module remove darshan']
-        self.postrun_cmds  = ['source delete_dirs.sh']
-        self.time_limit = '1h'
-        self.build_system = 'CMake'
-        self.build_system.ftn="ftn"
-        self.modules = [ "cray-hdf5-parallel" ]
-        
-
-        self.perf_patterns = {
-            'fullstriped_hdf5': sn.extractsingle(r'Writing to fullstriped/hdf5\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)',
-                                     self.stdout, 1, float),
-            'unstriped_hdf5': sn.extractsingle(r'Writing to unstriped/hdf5\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)',
-                                     self.stdout, 1, float),
-            'unstriped_mpiio': sn.extractsingle(r'Writing to unstriped/mpiio\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)',
-                                    self.stdout, 1, float),
-            'fullstriped_mpiio': sn.extractsingle(r'Writing to fullstriped/mpiio\.dat\W*\n\W*time\W*=\W*\d+.\d*\W*,\W*rate\W*=\W*(\d+.\d*)',
-                                    self.stdout, 1, float)
-        }
-
-        self.set_references_per_node()
-
-        
-    @run_before('run')
+    @run_before("run")
     def setup_run(self):
-        stagedir_name=os.path.split( self.stagedir )[-1]
-        self.env_vars["WRITE_DIR"]=os.path.join(self.write_dir_prefix,stagedir_name)
-        self.executable= self.stagedir + "/src/benchio"
+        """Setup test directory"""
+        stagedir_name = os.path.split(self.stagedir)[-1]
+        self.env_vars["WRITE_DIR"] = os.path.join(self.write_dir_prefix, stagedir_name)
+        self.executable = self.stagedir + "/src/benchio"
 
-    @run_before('compile')
+    @run_before("compile")
     def set_compiler_flags(self):
+        """Setup compiler flags"""
+        self.build_system.config_opts = ["-DUSE_HDF5=TRUE"]
 
-        self.build_system.config_opts= ["-DUSE_HDF5=TRUE" ]
-    
     @sanity_function
     def assert_benchio(self):
-        return sn.assert_found(r'Finished', self.stdout)
+        """Sanity checks"""
+        return sn.assert_found(r"Finished", self.stdout)
--- tests/synth/dstream/dstream.py
+++ tests/synth/dstream/dstream.py
@@ -1,88 +1,95 @@
+#!/usr/bin/env python3
+"""
+Distributed STREAM
+
+Test that runs STREAM in parallel to measure memory performance of many-core
+compute nodes. The code used is a modified version of DitributedStream from
+https://github.com/adrianjhpc/DistributedStream which was originally written
+by Adrian Jackson, EPCC. The modification removed the dependence on the MXML
+library to provide a simpler test program
+"""
+
 import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-# Distributed STREAM
-#
-# Test that runs STREAM in parallel to measure memory performance of many-core
-# compute nodes. The code used is a modified version of DitributedStream from
-# https://github.com/adrianjhpc/DistributedStream which was originally written
-# by Adrian Jackson, EPCC. The modification removed the dependence on the MXML
-# library to provide a simpler test program
-#
-
-
 @rfm.simple_test
 class StreamTest(rfm.RegressionTest):
-    def __init__(self):
-        self.valid_systems = ['archer2:compute','cirrus:compute']
-        self.valid_prog_environs = ['*']
-        self.build_system = 'Make'
-        self.executable = 'distributed_streams'
-        self.use_multithreading = False
-        self.sanity_patterns = sn.assert_found(r'Node Triad',
-                                               self.stdout)
-        self.perf_patterns = {
-                'Copy': sn.extractsingle(r'Node Copy:(\s+\S+:){2}\s+(?P<val>\S+):',
-                                     self.stdout, 'val', float, item=-1),
-                'Scale': sn.extractsingle(r'Node Scale:(\s+\S+:){2}\s+(?P<val>\S+):',
-                                     self.stdout, 'val', float, item=-1),
-                'Add': sn.extractsingle(r'Node Add:(\s+\S+:){2}\s+(?P<val>\S+):',
-                                     self.stdout, 'val', float, item=-1),
-                'Triad': sn.extractsingle(r'Node Triad:(\s+\S+:){2}\s+(?P<val>\S+):',
-                                     self.stdout, 'val', float, item=-1),
-        }
-        self.reference = {
-            'archer2:compute': {
-                'Copy':  (208600, -0.05, 0.05, 'MB/s'),
-                'Scale': (199000, -0.05, 0.05, 'MB/s'),
-                'Add':   (215700, -0.05, 0.05, 'MB/s'),
-                'Triad': (219400, -0.05, 0.05, 'MB/s')
-            },
-            'cirrus:compute': {
-                'Copy':  (88898, -0.05, 0.05, 'MB/s'),
-                'Scale': (84743, -0.05, 0.05, 'MB/s'),
-                'Add':   (93845, -0.05, 0.05, 'MB/s'),
-                'Triad': (97291, -0.05, 0.05, 'MB/s'),
-            }
+    """Stream test class"""
 
-        }
+    valid_systems = ["archer2:compute", "cirrus:compute"]
+    valid_prog_environs = ["*"]
+    build_system = "Make"
+    executable = "distributed_streams"
+    use_multithreading = False
 
-        # System specific settings
-        self.ntasks = {
-            'archer2:compute': 128,
-            'cirrus:compute': 36
-        }
-        self.ntasks_per_node = {
-            'archer2:compute': 128,
-            'cirrus:compute': 36
-        }
-        # These are the arguments to DistributedStream itself:
-        #   arg1: number of elements in each array created. Should exceed the size of
-        #         the highest cache level. (Arrays are double precision.)
-        #   arg2: the number of repetitions of the benchmark
+    reference = {
+        "archer2:compute": {
+            "Copy": (208600, -0.05, 0.05, "MB/s"),
+            "Scale": (199000, -0.05, 0.05, "MB/s"),
+            "Add": (215700, -0.05, 0.05, "MB/s"),
+            "Triad": (219400, -0.05, 0.05, "MB/s"),
+        },
+        "cirrus:compute": {
+            "Copy": (88898, -0.05, 0.05, "MB/s"),
+            "Scale": (84743, -0.05, 0.05, "MB/s"),
+            "Add": (93845, -0.05, 0.05, "MB/s"),
+            "Triad": (97291, -0.05, 0.05, "MB/s"),
+        },
+    }
 
-        # Cirrus L3 cache is 45 MiB
+    # System specific settings
+    ntasks = {"archer2:compute": 128, "cirrus:compute": 36}
+    ntasks_per_node = {"archer2:compute": 128, "cirrus:compute": 36}
+    # These are the arguments to DistributedStream it
+    #   arg1: number of elements in each array created. Should exceed the size of
+    #         the highest cache level. (Arrays are double precision.)
+    #   arg2: the number of repetitions of the benchmark
 
-        self.args = {
-            'archer2:compute': ['24000000','1000'],
-            'cirrus:compute': ['4500000','1000'],
+    # Cirrus L3 cache is 45 MiB
 
-        }
+    args = {
+        "archer2:compute": ["24000000", "1000"],
+        "cirrus:compute": ["4500000", "1000"],
+    }
 
-        self.maintainers = ['a.turner@epcc.ed.ac.uk']
-        self.tags = {'performance','short'}
+    maintainers = ["a.turner@epcc.ed.ac.uk"]
+    tags = {"performance", "short"}
 
-    @run_before('run')
+    @performance_function("MB/s", perf_key="Copy")
+    def extract_copy(self):
+        """Extract copy performance value"""
+        return sn.extractsingle(r"Node Copy:(\s+\S+:){2}\s+(?P<val>\S+):", self.stdout, "val", float, item=-1)
+
+    @performance_function("MB/s", perf_key="Scale")
+    def extract_scale(self):
+        """Extract scale performance value"""
+        return sn.extractsingle(r"Node Scale:(\s+\S+:){2}\s+(?P<val>\S+):", self.stdout, "val", float, item=-1)
+
+    @performance_function("MB/s", perf_key="Add")
+    def extract_add(self):
+        """Extract add performance value"""
+        return sn.extractsingle(r"Node Add:(\s+\S+:){2}\s+(?P<val>\S+):", self.stdout, "val", float, item=-1)
+
+    @performance_function("MB/s", perf_key="Triad")
+    def extract_triad(self):
+        """Extract triad performance value"""
+        return sn.extractsingle(r"Node Triad:(\s+\S+:){2}\s+(?P<val>\S+):", self.stdout, "val", float, item=-1)
+
+    @sanity_function
+    def assert_benchio(self):
+        """Sanity checks"""
+        return sn.assert_found(r"Node Triad", self.stdout)
+
+    @run_before("run")
     def set_num_threads(self):
+        """sets number of threads"""
         num_tasks = self.ntasks.get(self.current_partition.fullname, 1)
         self.num_tasks = num_tasks
         num_tasks_per_node = self.ntasks_per_node.get(self.current_partition.fullname, 1)
         self.num_tasks_per_node = num_tasks_per_node
         self.num_cpus_per_task = 1
-        self.time_limit = '20m'
-        args = self.args.get(self.current_partition.fullname, ['24000000','10000'])
-        self.extra_resources = {
-                'qos': {'qos': 'standard'}
-        }
+        self.time_limit = "20m"
+        args = self.args.get(self.current_partition.fullname, ["24000000", "10000"])
+        self.extra_resources = {"qos": {"qos": "standard"}}
         self.executable_opts = args
