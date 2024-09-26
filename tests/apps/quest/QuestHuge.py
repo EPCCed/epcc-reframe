a2181c614083e7d0ad3f18c57c99183a56cef229
JPRichings <j.richings@epcc.ed.ac.uk>
2024-09-26 12:11:10 +0100

    Added additional version of Quest test for varrying scales

--- tests/apps/quest/QuestHuge.py
+++ tests/apps/quest/QuestHuge.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-"""Reframe test for XCompact3D"""
-
 # Based on original work from:
 #   Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 #   ReFrame Project Developers. See the top-level LICENSE file for details.
@@ -20,14 +18,14 @@ class QuestQFTHugeTest(rfm.RegressionTest):
 
     tags = {"performance", "largescale", "applications"}
 
-    num_nodes = 512
+    num_nodes = 4096
     num_tasks_per_node = 1
     num_cpus_per_task = 128
-    num_tasks = num_nodes * num_tasks_per_node 
+    num_tasks = num_nodes * num_tasks_per_node
 
     env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task), "OMP_PLACES": "cores"}
 
-    time_limit = "20m"
+    time_limit = "1hr"
     build_system = "Make"
     prebuild_cmds = [
         "git clone https://github.com/QuEST-Kit/QuEST.git",
@@ -35,15 +33,14 @@ class QuestQFTHugeTest(rfm.RegressionTest):
     ]
     builddir = "build"
     executable = "build/qft"
-    executable_opts = ["41"]
+    executable_opts = ["44"]
     modules = ["cmake/3.29.4"]
-
 
     @run_before("compile")
     def prepare_build(self):
         self.prebuild_cmd = ["git clone https://github.com/QuEST-Kit/QuEST.git .", "source cmake_questQFT.sh qft"]
-    
-    @run_before('run')
+
+    @run_before("run")
     def set_num_nodes(self):
         self.job.options = [f"--nodes={self.num_nodes}"]
 
@@ -52,12 +49,13 @@ class QuestQFTHugeTest(rfm.RegressionTest):
         """Sanity check that simulation finished successfully"""
         return sn.assert_found("Total run time:", self.stdout)
 
-   # @performance_function("seconds", perf_key="performance")
-   # def extract_perf(self):
-   #     """Extract performance value to compare with reference value"""
-   #     return sn.extractsingle(
-   #         r"Averaged time per step \(s\):\s+(?P<steptime>\S+)",
-   #         self.stdout,
-   #         "time",
-   #         float,
-   #     )
+
+# @performance_function("seconds", perf_key="performance")
+# def extract_perf(self):
+#     """Extract performance value to compare with reference value"""
+#     return sn.extractsingle(
+#         r"Averaged time per step \(s\):\s+(?P<steptime>\S+)",
+#         self.stdout,
+#         "time",
+#         float,
+#     )
---
+++ tests/apps/quest/QuestLarge.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Based on original work from:
+#   Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+#   ReFrame Project Developers. See the top-level LICENSE file for details.
+#   SPDX-License-Identifier: BSD-3-Clause
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class QuestQFTLargeTest(rfm.RegressionTest):
+    """Quest Large 512 node Test"""
+
+    valid_systems = ["archer2:compute-capability"]
+    valid_prog_environs = ["PrgEnv-gnu"]
+
+    tags = {"performance", "largescale", "applications"}
+
+    num_nodes = 512
+    num_tasks_per_node = 1
+    num_cpus_per_task = 128
+    num_tasks = num_nodes * num_tasks_per_node
+
+    env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task), "OMP_PLACES": "cores"}
+
+    time_limit = "20m"
+    build_system = "Make"
+    prebuild_cmds = [
+        "git clone https://github.com/QuEST-Kit/QuEST.git",
+        "source cmake_questQFT.sh qft",
+    ]
+    builddir = "build"
+    executable = "build/qft"
+    executable_opts = ["41"]
+    modules = ["cmake/3.29.4"]
+
+    @run_before("compile")
+    def prepare_build(self):
+        self.prebuild_cmd = ["git clone https://github.com/QuEST-Kit/QuEST.git .", "source cmake_questQFT.sh qft"]
+
+    @run_before("run")
+    def set_num_nodes(self):
+        self.job.options = [f"--nodes={self.num_nodes}"]
+
+    @sanity_function
+    def assert_finished(self):
+        """Sanity check that simulation finished successfully"""
+        return sn.assert_found("Total run time:", self.stdout)
+
+
+# @performance_function("seconds", perf_key="performance")
+# def extract_perf(self):
+#     """Extract performance value to compare with reference value"""
+#     return sn.extractsingle(
+#         r"Averaged time per step \(s\):\s+(?P<steptime>\S+)",
+#         self.stdout,
+#         "time",
+#         float,
+#     )
