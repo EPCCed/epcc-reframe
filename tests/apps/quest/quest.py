46f83ce1ac8778cceab2c086aaf86e0ca312282a
JPRichings <j.richings@epcc.ed.ac.uk>
2024-09-26 11:58:04 +0100

    quest qft capability job tested in this config 512 nodes

--- tests/apps/quest/quest.py
+++ tests/apps/quest/quest.py
@@ -15,19 +15,19 @@ import reframe.utility.sanity as sn
 class QuestQFTHugeTest(rfm.RegressionTest):
     """Quest Huge 4096 node Test"""
 
-    valid_systems = ["archer2:compute"]
+    valid_systems = ["archer2:compute-capability"]
     valid_prog_environs = ["PrgEnv-gnu"]
 
     tags = {"performance", "largescale", "applications"}
 
-    num_nodes = 4
+    num_nodes = 512
     num_tasks_per_node = 1
     num_cpus_per_task = 128
     num_tasks = num_nodes * num_tasks_per_node 
 
     env_vars = {"OMP_NUM_THREADS": str(num_cpus_per_task), "OMP_PLACES": "cores"}
 
-    time_limit = "10m"
+    time_limit = "20m"
     build_system = "Make"
     prebuild_cmds = [
         "git clone https://github.com/QuEST-Kit/QuEST.git",
@@ -35,13 +35,17 @@ class QuestQFTHugeTest(rfm.RegressionTest):
     ]
     builddir = "build"
     executable = "build/qft"
-    executable_opts = ["5"]
+    executable_opts = ["41"]
     modules = ["cmake/3.29.4"]
 
 
     @run_before("compile")
     def prepare_build(self):
         self.prebuild_cmd = ["git clone https://github.com/QuEST-Kit/QuEST.git .", "source cmake_questQFT.sh qft"]
+    
+    @run_before('run')
+    def set_num_nodes(self):
+        self.job.options = [f"--nodes={self.num_nodes}"]
 
     @sanity_function
     def assert_finished(self):
