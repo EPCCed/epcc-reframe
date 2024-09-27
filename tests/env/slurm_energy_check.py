9bd8a36e4f609698f92b0e35d27356ec6854cd7a
JPRichings <j.richings@epcc.ed.ac.uk>
2024-09-27 16:58:16 +0100

    tidy up test and remove printing

--- tests/env/slurm_energy_check.py
+++ tests/env/slurm_energy_check.py
@@ -20,58 +20,43 @@ class SlurmEnergyTest(rfm.RunOnlyRegressionTest):
     executable = "./energy_diff.sh"
 
     tags = {"production", "maintenance", "craype"}
-  
 
-    reference = {"archer2:compute": {"energy": (0, -1, 1, "jouls")}}
-
+    reference = {"archer2:compute": {"energy diff": (0, -1, 1, "jouls")}}
 
     @sanity_function
     def assert_finished(self):
         """Sanity check that SLURM_CPU_FREQ_REQ is set"""
         return sn.assert_found(f"\S+ J \S+ us", self.stdout)
 
-    @run_after("run")
-    def slurm_energy_result(self):
-        """Print energy usage captured by slurm"""
-        jobid= self.job.jobid
-        print("jobid: ", jobid)
-        slurm = rfm.utility.osext.run_command("sacct -j " + str(jobid) + " --format=User,JobID,nnodes,elapsed,ReqCPUFreq,ConsumedEnergy", check=True, shell=True)
-        print("slurm: ", slurm)
-        return 0
-
-
-    @performance_function("jouls diff", perf_key="performance")
+    @performance_function("energy diff", perf_key="performance")
     def extract_perf(self):
-        """Extract performance value to compare with reference value"""
-        jobid= self.job.jobid
-        print("jobid: ", jobid)
-        slurm = rfm.utility.osext.run_command("sacct -j " + str(jobid) + " --format=JobID,ConsumedEnergy --noconvert | tr '\n' ' ' ", check=True, shell=True)
-        print("slurm: ", slurm.stdout)
-
-        energy_counters = sn.extractall(
-            r"(?P<energy>[0-9]+)\sJ\s(?P<time>[0-9]+)\sus",
-            self.stdout,
-            "energy"
+        """Extract energy from counters to compare with slurm and check diff is zero"""
+        jobid = self.job.jobid
+        #print("jobid: ", jobid)
+        slurm = rfm.utility.osext.run_command(
+            "sacct -j " + str(jobid) + " --format=JobID,ConsumedEnergy --noconvert | tr '\n' ' ' ",
+            check=True,
+            shell=True,
         )
+        # print("slurm: ", slurm.stdout)
 
-        print("energy counters: ", energy_counters)
+        energy_counters = sn.extractall(r"(?P<energy>[0-9]+)\sJ\s(?P<time>[0-9]+)\sus", self.stdout, "energy")
+
+        # print("energy counters: ", energy_counters)
 
         energy_slurm = sn.extractall_s(
             r"JobID\s+ConsumedEnergy\s+------------ --------------\s+[0-9]+\s+[0-9]+\s+[0-9]+.bat\+\s+[0-9]+\s+[0-9]+.ext\+\s+[0-9]+\s+[0-9]+.0\s+(?P<energy>[0-9]+)",
             str(slurm.stdout),
-            "energy"
+            "energy",
         )
-        print("energy slurm: ", energy_slurm)
-    
-        
+        # print("energy slurm: ", energy_slurm)
+
         energy_counters_diff = int(str(energy_counters[1])) - int(str(energy_counters[0]))
 
-        print("energy counters diff: ", energy_counters_diff)
+        # print("energy counters diff: ", energy_counters_diff)
 
         diff = energy_counters_diff - int(str(energy_slurm[0]))
 
-        print("diff: ", diff)
+        # print("diff: ", diff)
 
         return diff
-        
-
