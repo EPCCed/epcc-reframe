a799a97a877176b27c270f0fa3bdf64ca7d81aa9
lparisi <l.parisi@epcc.ed.ac.uk>
2026-08-11 10:32:59 +0100

    Removed references to the yaml files, now obsolete

--- tests/synth/cbenchio/cbenchio_benchmarks.py
+++ tests/synth/cbenchio/cbenchio_benchmarks.py
@@ -2,25 +2,6 @@
 
 Contains I/O benchmarks based on cbenchio.
 
-## Adding new tests
-
-Add a new test by inherting from cbenchio_write and using the Parameterize metaclass.
-The class should have a config attribute pointing to a yaml file.
-The yaml file should contain the parameters for the test. For example:
-
-```python
-class cbenchio_bandwidth_write(cbenchio_write,metaclass=Parameterize):
-    operation = "write"
-    config= "posix.yaml"
-```
-
-Read tests can be generated from write tests using the make_read_test function.
-For instance we can generate a read test from the write test above using:
-
-```python
-cbenchio_bandwidth_read = make_read_test(cbenchio_bandwidth_write)
-```
-
 """
 
 from cbenchio import CbenchioWrite, make_read_test
@@ -46,7 +27,6 @@ class CbenchioPosixRandomWrite(CbenchioWrite):
     """Measure the bandwidth of the filesystem for file per process patterns random 4KiB I/O."""
 
     operation = "write"
-    config = "posix-random.yaml"
     random_strided = True
     fields = 4
 
@@ -88,7 +68,6 @@ class CbenchioMpi3DWrite(CbenchioWrite):
     """Measure the bandwidth of the filesystem for a single file written by all processes in a 3D layout."""
 
     operation = "write"
-    config = "mpi-3D.yaml"
     file_per_process = False
     dimensions = 3
     fields = 4
